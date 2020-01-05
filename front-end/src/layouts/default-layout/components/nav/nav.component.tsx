bb002f287d6cb300689f51bf3e3c4573fa5b2067
tronjsdev <tronjs.dev@gmail.com>
2020-01-05 20:42:34 +0700

    Lesson 26

--- front-end/src/layouts/default-layout/components/nav/nav.component.tsx
+++ front-end/src/layouts/default-layout/components/nav/nav.component.tsx
@@ -7,26 +7,30 @@ const Nav = () => {
   const user = useUser();
   return (
     <NavStyles>
-      <p>{user && user.name}</p>
       <Link href="/items">
         <a>Shop</a>
       </Link>
 
-      <Link href="/sell">
-        <a>Sell</a>
-      </Link>
+      {user && (
+        <>
+          <Link href="/sell">
+            <a>Sell</a>
+          </Link>
 
-      <Link href="/signup">
-        <a>Signup</a>
-      </Link>
+          <Link href="/orders">
+            <a>Orders</a>
+          </Link>
 
-      <Link href="/orders">
-        <a>Orders</a>
-      </Link>
-
-      <Link href="/me">
-        <a>Account</a>
-      </Link>
+          <Link href="/me">
+            <a>Account</a>
+          </Link>
+        </>
+      )}
+      {!user && (
+        <Link href="/signup">
+          <a>Sign In</a>
+        </Link>
+      )}
     </NavStyles>
   );
 };
